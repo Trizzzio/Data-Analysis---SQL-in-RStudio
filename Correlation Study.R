4c7f562e0a97e7719dfd5c8b7e054a8c3f4a7e5e
Patrick Thöni <patrick@thonigmail.com>
2025-01-17 11:25:07 +0100

    Correlation Study

--- Correlation Study.R
+++ Correlation Study.R
@@ -1,8 +1,9 @@
 ##Correlation Study
-
+library(tidyverse)
 library(dplyr)
 library(tidyr)
 library(lubridate)
+library(zoo)
 
 ##Query indices data
 
@@ -10,7 +11,7 @@ library(lubridate)
 indices_data <- dbGetQuery(conn,"
    SELECT symbol, date, close
    FROM indices_data
-   WHERE symbol IN ('^GDAXI', '^GSPC', '^FTSE', '^IXIC')
+   WHERE symbol IN ('^GDAXI', '^GSPC', '^FTSE', '^DJI')
    ORDER BY DATE")
 
 
@@ -30,4 +31,45 @@ returns_wide <- indices_data %>%
   na.omit()
 
 
+
+
 #Calculating rolling correlations
+
+#Define rolling window size
+
+rolling_window <- 30
+
+#Calculate rolling correaltions
+
+returns_wide <- returns_wide %>%
+  mutate(
+    corr_dax_sp500 = rollapplyr(data=returns_wide[, c("^GDAXI", "^GSPC")], rolling_window, function(x) cor(x[,1], x[,2], use = "complete.obs"),by.column = FALSE,fill=NA),
+    corr_dax_dji = rollapplyr(data=returns_wide[, c("^GDAXI", "^DJI")], rolling_window, function(x) cor(x[,1], x[,2], use = "complete.obs"),by.column = FALSE,fill=NA)
+  )
+
+
+##Visualize
+
+#Pivor long
+
+correlations <- returns_wide %>%
+  select(date, corr_dax_sp500, corr_dax_dji ) %>%
+  pivot_longer(-date, names_to = "pair", values_to = "correlations")%>%
+  filter(date>=as.Date("2020-02-14"))
+
+#Plot
+
+ggplot(correlations, aes(x = date, y = correlations, color = pair)) +
+  geom_line(size = 1) +
+  labs(
+    title = "Rolling Correlations Between Index Pairs",
+    subtitle = "30-Day Rolling Window",
+    x = "Date",
+    y = "Correlation",
+    color = "Index Pair"
+  ) +
+  theme_minimal() +
+  scale_color_manual(values = c("corr_dax_sp500" = "blue", 
+                                "corr_dax_dji" = "red"))
+
+
